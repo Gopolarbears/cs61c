29069afaae736f379d7c4051649c06e8addb04da
John Doe <3160231312@qq.com>
2024-09-08 23:00:36 +0800

    lab01 completed

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL || head->next == NULL || head->next->next == NULL) {
+        return 0;
+    }
+    node* hare = head->next->next;
+    node* tortoise = head;
+    while (hare != NULL && hare->next != NULL) {
+        if (hare == tortoise) {
+            return 1;
+        }
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+    }
     return 0;
 }
