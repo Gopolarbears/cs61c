eb9e8e37bd03d00f50cc6509ddffe64f41d2f892
John Doe <3160231312@qq.com>
2024-09-22 21:42:03 +0800

    exercis3

--- lab02/vector.c
+++ lab02/vector.c
@@ -58,27 +58,27 @@ vector_t *vector_new() {
     vector_t *retval;
 
     /* First, we need to allocate memory on the heap for the struct */
-    retval = /* YOUR CODE HERE */
+    retval = malloc(sizeof(vector_t));/* YOUR CODE HERE */
 
     /* Check our return value to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
+    if (/* YOUR CODE HERE */ retval == NULL) {
         allocation_failed();
     }
 
     /* Now we need to initialize our data.
        Since retval->data should be able to dynamically grow,
        what do you need to do? */
-    retval->size = /* YOUR CODE HERE */;
-    retval->data = /* YOUR CODE HERE */;
+    retval->size = /* YOUR CODE HERE */ 1;
+    retval->data = /* YOUR CODE HERE */ calloc(retval->size, sizeof(int));
 
     /* Check the data attribute of our vector to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
+    if (/* YOUR CODE HERE */ retval->data == NULL) {
         free(retval);				//Why is this line necessary?
         allocation_failed();
     }
 
     /* Complete the initialization by setting the single component to zero */
-    /* YOUR CODE HERE */ = 0;
+    /* YOUR CODE HERE */ *(retval->data)= 0;
 
     /* and return... */
     return retval;
@@ -96,8 +96,8 @@ int vector_get(vector_t *v, size_t loc) {
     /* If the requested location is higher than we have allocated, return 0.
      * Otherwise, return what is in the passed location.
      */
-    if (loc < /* YOUR CODE HERE */) {
-        return /* YOUR CODE HERE */;
+    if (loc < /* YOUR CODE HERE */v->size) {
+        return /* YOUR CODE HERE */(v->data)[loc];
     } else {
         return 0;
     }
@@ -106,7 +106,10 @@ int vector_get(vector_t *v, size_t loc) {
 /* Free up the memory allocated for the passed vector.
    Remember, you need to free up ALL the memory that was allocated. */
 void vector_delete(vector_t *v) {
-    /* YOUR SOLUTION HERE */
+    if (v != NULL) {
+        free(v->data);  // Free the allocated memory for data
+        free(v);        // Free the allocated memory for the vector struct
+    }
 }
 
 /* Set a value in the vector. If the extra memory allocation fails, call
@@ -117,4 +120,17 @@ void vector_set(vector_t *v, size_t loc, int value) {
      */
 
     /* YOUR SOLUTION HERE */
+    if (loc < v->size) {
+        *(v->data + loc) = value;
+        return;
+    }
+    
+    int* newData = calloc(loc + 1, sizeof(int));
+    for (int i = 0; i < v->size; i++) {
+        *(newData + i) = *(v->data + i);
+    }
+    *(newData + loc) = value;
+    free(v->data);
+    v->size = loc + 1;
+    v->data = newData;
 }
